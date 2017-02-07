284f645028e234d881b648617e9f7d9c56c2b022
HeroDishonest <xanderxander2015@yandex.ru>
2017-02-08 02:08:18 +0300

    commit

--- src/by/pvt/heldyieu/dao/connection/ConnectionFactory.java
+++ src/by/pvt/heldyieu/dao/connection/ConnectionFactory.java
@@ -1,42 +1,40 @@
 package by.pvt.heldyieu.dao.connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 import org.apache.log4j.Logger;
+import by.pvt.heldyieu.resources.ResourceManager;
 
 public class ConnectionFactory {
 	
 	private static final Logger LOGGER = Logger.getLogger(ConnectionFactory.class);
-	private static ConnectionFactory connectionFactory = null;
-    private DataSource dataSource = null;
-
-    private ConnectionFactory() {
-        LOGGER.info("Initializing connectionFactory class");
-
-        try {
-            Context initCtx = new InitialContext();
-            Context envCtx = (Context) initCtx.lookup("java:comp/env");
-
-            dataSource = (DataSource) envCtx.lookup("jdbc/periodicals");
-        } catch (NamingException e) {
-            LOGGER.error(e.getMessage());
-        }
-    }
-
-    public Connection getConnection() throws SQLException {
-        LOGGER.info("Getting connection");
-        return dataSource.getConnection();
-    }
-
-    public static synchronized ConnectionFactory getInstance() {
-        if(connectionFactory == null) {
-            connectionFactory = new ConnectionFactory();
-        }
-        return connectionFactory;
-    }
+	 	private static ConnectionFactory connectionFactory;
+	 	private static ResourceManager resmanager = new ResourceManager("connection");
+	  	private Connection connect;
+	 	// TODO NOT SINGLETON
+	     public ConnectionFactory() {
+	          LOGGER.info("Initializing connectionFactory class");
+	  
+	          try {
+	         	resmanager = new ResourceManager("connection");
+	          	Class.forName(resmanager.getProperty("jdbc"));
+	          	connect = DriverManager.getConnection(resmanager.getProperty("database"), resmanager.getProperty("user"), resmanager.getProperty("password"));
+	          	}catch (ClassNotFoundException | SQLException e) {
+	                LOGGER.error(e.getMessage());
+	            }
+	        }
+	          	
+	 public Connection getConnection() throws SQLException {
+	          LOGGER.info("Getting connection");
+	          return connect;
+	      }
+	 
+	     public static synchronized ConnectionFactory getInstance() {
+	         if(connectionFactory == null) {
+	             connectionFactory = new ConnectionFactory();
+	         }
+	         return connectionFactory;
+	     }
     
 }
